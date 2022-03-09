c207630ca810bd144bcfbbc6dd89fcb9873aa2df
singfie <fietekrutein@gmail.com>
2022-03-08 17:39:02 -0800

    revised experiment generation method to fit RH-ICEP

--- experiment_generator.py
+++ experiment_generator.py
@@ -39,11 +39,49 @@ def diversify_uncertain_experiments():
     if not os.path.exists(os.path.join(dirname, 'experiment_files')):
         os.makedirs(os.path.join(dirname, 'experiment_files'))
 
+    # read in demand
+    demand = pd.read_csv(os.path.join(path, 'input', 'scenarios.csv'))
+
     # make variations
     for ratio in [2, 3, 4]: # ratio = (total demand)/(total capacity of resources)
+        # randomly distribute the initial demand estimate across locations
+        real_distribution = np.random.dirichlet(np.ones(len(demand)),size=1)
         for variance_factor in [0.2, 0.4, 0.6]: # variance_factor = (std_deviation over time)/(total demand)
-            for time_interval in [15, 30, 60]: # time in between updates
-                for number_updates in [3, 5, 10]: # number of information updates
+            for number_updates in [3, 5, 10]: # number of information updates
+
+                # read in demand
+                demand = pd.read_csv(os.path.join(path, 'input', 'scenarios.csv'))
+                # read in resources
+                resources = pd.read_csv(os.path.join(path, 'input', 'vessels.csv'))
+
+                # calculating metrics
+                total_capacity = resources['max_cap'].sum()
+
+                # resulting demand figure
+                total_demand = ratio * total_capacity
+
+                ### DEMAND RATIO ###
+                demand = demand[['Scenario','Location','private_evac','Actual_demand']]
+
+                for i in range(len(demand)):
+                    demand['Actual_demand'].iloc[i] = max(int(np.round(real_distribution[0][i] * total_demand)),0)
+
+                ### NUMBER OF UPDATES AND VARIANCE FACTOR ###
+                for i in range(number_updates):
+                    demand['Demand_' + str(i)] = 0
+                    for j in range(len(demand)):
+                        demand['Demand_' + str(i)].iloc[j] = max(int(np.round(np.random.normal(loc=demand['Actual_demand'].iloc[j],
+                                                                                               scale=variance_factor * demand['Actual_demand'].iloc[j]))),0)
+
+                # update the actual demand
+                demand['Robust_demand'] = 0
+                demand['Demand_' + str(number_updates)] = 0
+                for j in range(len(demand)):
+                    demand_columns = demand.loc[:, demand.columns.str.startswith('Demand_')]
+                    demand['Robust_demand'].iloc[j] = max(demand_columns.iloc[j])
+                    demand['Demand_' + str(number_updates)].iloc[j] = demand['Actual_demand'].iloc[j]
+
+                for time_interval in [15, 30, 60]: # time in between updates
                     # create a file copy
                     new_file_path = os.path.join(dirname, 'experiment_files', 'experiment_' + rel_path.split('/')[1] +
                                                  '_ratio_' + str(ratio) +
@@ -52,40 +90,6 @@ def diversify_uncertain_experiments():
                                                  '_number_updates_' + str(number_updates) +
                                                  '_seed_' + str(seed))
                     shutil.copytree(path, new_file_path)
-
-                    # read in demand
-                    demand = pd.read_csv(os.path.join(path, new_file_path, 'input', 'scenarios.csv'))
-                    # read in resources
-                    resources = pd.read_csv(os.path.join(path, new_file_path, 'input', 'vessels.csv'))
-
-                    # calculating metrics
-                    total_capacity = resources['max_cap'].sum()
-
-                    # resulting demand figure
-                    total_demand = ratio * total_capacity
-
-                    ### DEMAND RATIO ###
-                    demand = demand[['Scenario','Location','private_evac','Demand_0']]
-                    # randomly distribute the initial demand estimate across locations
-                    distribution = np.random.dirichlet(np.ones(len(demand)),size=1)
-                    for i in range(len(demand)):
-                        demand['Demand_0'].iloc[i] = max(int(np.round(distribution[0][i] * total_demand)),0)
-
-                    ### NUMBER OF UPDATES AND VARIANCE FACTOR ###
-                    for i in range(1, number_updates + 1):
-                        demand['Demand_' + str(i)] = 0
-                        for j in range(len(demand)):
-                            demand['Demand_' + str(i)].iloc[j] = max(int(np.round(np.random.normal(loc=demand['Demand_0'].iloc[j],
-                                                                                  scale=variance_factor * demand['Demand_0'].iloc[j]))),0)
-
-                    # update the actual demand
-                    demand['Robust_demand'] = 0
-                    demand['Actual_demand'] = 0
-                    for j in range(len(demand)):
-                        demand_columns = demand.loc[:, demand.columns.str.startswith('Demand_')]
-                        demand['Robust_demand'].iloc[j] = max(demand_columns.iloc[j])
-                        demand['Actual_demand'].iloc[j] = demand['Demand_' + str(number_updates)].iloc[j]
-                    # update the robust demand
                     demand.to_csv(os.path.join(path, new_file_path, 'input', 'scenarios.csv'), index = False)
 
     return(-1)
